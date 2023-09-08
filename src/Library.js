e533eb24737b4a6bf0a80c9c1d5c9db3ad2aa428
eholster <ehols001@odu.edu>
2023-09-08 12:16:34 -0400

    fixed card mapping for display

--- src/Library.js
+++ src/Library.js
@@ -8,14 +8,8 @@ function Library() {
   * 
   */ 
 
-  const [heroData, setHeroData] = React.useState(null);
-  const [minionData, setMinionData] = React.useState(null);
-  const [tier1Data, setTier1Data] = React.useState(null);
-  const [tier2Data, setTier2Data] = React.useState(null);
-  const [tier3Data, setTier3Data] = React.useState(null);
-  const [tier4Data, setTier4Data] = React.useState(null);
-  const [tier5Data, setTier5Data] = React.useState(null);
-  const [tier6Data, setTier6Data] = React.useState(null);
+  const [heroData, setHeroData] = useState(null);
+  const [minionData, setMinionData] = useState(null);
 
   React.useEffect(() => {
     fetch("/heroes")
@@ -29,42 +23,6 @@ function Library() {
       .then((minionData) => setMinionData(minionData));
   }, []);
 
-  React.useEffect(() => {
-    fetch("/tier-1")
-      .then((res) => res.json())
-      .then((tier1Data) => setTier1Data(tier1Data));
-  }, []);
-
-  React.useEffect(() => {
-    fetch("/tier-2")
-      .then((res) => res.json())
-      .then((tier2Data) => setTier2Data(tier2Data));
-  }, []);
-
-  React.useEffect(() => {
-    fetch("/tier-3")
-      .then((res) => res.json())
-      .then((tier3Data) => setTier3Data(tier3Data));
-  }, []);
-
-  React.useEffect(() => {
-    fetch("/tier-4")
-      .then((res) => res.json())
-      .then((tier4Data) => setTier4Data(tier4Data));
-  }, []);
-
-  React.useEffect(() => {
-    fetch("/tier-5")
-      .then((res) => res.json())
-      .then((tier5Data) => setTier5Data(tier5Data));
-  }, []);
-
-  React.useEffect(() => {
-    fetch("/tier-6")
-      .then((res) => res.json())
-      .then((tier6Data) => setTier6Data(tier6Data));
-  }, []);
-
   /*
   *
   * Mapping and hero and minion data to display.
@@ -73,12 +31,6 @@ function Library() {
 
   const [heroes, setHeroes] = useState([]);
   const [minions, setMinions] = useState([]);
-  const [tier1Minions, setTier1Minions] = React.useState(null);
-  const [tier2Minions, setTier2Minions] = React.useState(null);
-  const [tier3Minions, setTier3Minions] = React.useState(null);
-  const [tier4Minions, setTier4Minions] = React.useState(null);
-  const [tier5Minions, setTier5Minions] = React.useState(null);
-  const [tier6Minions, setTier6Minions] = React.useState(null);
 
   React.useEffect(() => {
     if(heroData) {
@@ -113,131 +65,15 @@ function Library() {
     }
   }, [minionData]);
 
-  React.useEffect(() => {
-    if(tier1Data) {
-      setTier1Minions(
-        tier1Data.map((minion) => {
-          return (
-            <div key={minion.id} className="col-2 text-center">
-              <figure className="figure">
-                <img src={minion.battlegrounds.image} className="figure-img img-fluid" alt="card-display" />
-              </figure>
-            </div>
-          );
-        })
-      );
-    }
-  }, [tier1Data]);
-
-  React.useEffect(() => {
-    if(tier2Data) {
-      setTier2Minions(
-        tier2Data.map((minion) => {
-          return (
-            <div key={minion.id} className="col-2 text-center">
-              <figure className="figure">
-                <img src={minion.battlegrounds.image} className="figure-img img-fluid" alt="card-display" />
-              </figure>
-            </div>
-          );
-        })
-      );
-    }
-  }, [tier2Data]);
-
-  React.useEffect(() => {
-    if(tier3Data) {
-      setTier3Minions(
-        tier3Data.map((minion) => {
-          return (
-            <div key={minion.id} className="col-2 text-center">
-              <figure className="figure">
-                <img src={minion.battlegrounds.image} className="figure-img img-fluid" alt="card-display" />
-              </figure>
-            </div>
-          );
-        })
-      );
-    }
-  }, [tier3Data]);
-
-  React.useEffect(() => {
-    if(tier4Data) {
-      setTier4Minions(
-        tier4Data.map((minion) => {
-          return (
-            <div key={minion.id} className="col-2 text-center">
-              <figure className="figure">
-                <img src={minion.battlegrounds.image} className="figure-img img-fluid" alt="card-display" />
-              </figure>
-            </div>
-          );
-        })
-      );
-    }
-  }, [tier4Data]);
-
-  React.useEffect(() => {
-    if(tier5Data) {
-      setTier5Minions(
-        tier5Data.map((minion) => {
-          return (
-            <div key={minion.id} className="col-2 text-center">
-              <figure className="figure">
-                <img src={minion.battlegrounds.image} className="figure-img img-fluid" alt="card-display" />
-              </figure>
-            </div>
-          );
-        })
-      );
-    }
-  }, [tier5Data]);
-
-  React.useEffect(() => {
-    if(tier6Data) {
-      setTier6Minions(
-        tier6Data.map((minion) => {
-          return (
-            <div key={minion.id} className="col-2 text-center">
-              <figure className="figure">
-                <img src={minion.battlegrounds.image} className="figure-img img-fluid" alt="card-display" />
-              </figure>
-            </div>
-          );
-        })
-      );
-    }
-  }, [tier6Data]);
-
   return (
     <div className="container-fluid">
       <h3>~~~~~~~~~~ Heroes ~~~~~~~~~~</h3>
       <div className="row text-white card-section">
         {heroes}
       </div>
-      <h3>~~~~~~~~~~ Tavern Tier 1 ~~~~~~~~~~</h3>
+      <h3>~~~~~~~~~~ Minions ~~~~~~~~~~</h3>
       <div className="row text-white card-section">
-        {tier1Minions}
-      </div>
-      <h3>~~~~~~~~~~ Tavern Tier 2 ~~~~~~~~~~</h3>
-      <div className="row text-white card-section">
-        {tier2Minions}
-      </div>
-      <h3>~~~~~~~~~~ Tavern Tier 3 ~~~~~~~~~~</h3>
-      <div className="row text-white card-section">
-        {tier3Minions}
-      </div>
-      <h3>~~~~~~~~~~ Tavern Tier 4 ~~~~~~~~~~</h3>
-      <div className="row text-white card-section">
-        {tier4Minions}
-      </div>
-      <h3>~~~~~~~~~~ Tavern Tier 5 ~~~~~~~~~~</h3>
-      <div className="row text-white card-section">
-        {tier5Minions}
-      </div>
-      <h3>~~~~~~~~~~ Tavern Tier 6 ~~~~~~~~~~</h3>
-      <div className="row text-white card-section">
-        {tier6Minions}
+        {minions}
       </div>
     </div>
   );
